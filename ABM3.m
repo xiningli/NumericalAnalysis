0c9dd33b425e4abd676f598e469a593cdfac22c0
Xining Li <xiningli@umail.ucsb.edu>
2016-01-25 12:28:26 -0800

    Create ABM3.m

---
+++ ABM3.m
@@ -0,0 +1,19 @@
+function [x, y] = ABM3_RK3(f, y0, tspan, n)
+a=tspan(1); b = tspan(2);   h = (b-a)/n;    k = h/12;
+x = (a : h : b);    y(1)=y0;
+for i = 1:2
+    z(i) = f(x(i),y(i));
+    k1 = h*z(i);
+    k2 = h*f(x(i)+0.5*h, y(i) + 0.5 *k1);
+    k3 = h*f(x(i)+0.75*h, y(i) + 0.75*k2);
+    y(i+1) = y(i) + (2*k1+3*k2+4*k3)/9;
+end
+for i=3:n
+    z(i) = f(x(i),y(i));
+   
+    yy(i+1) = y(i) + k*(23*z(i)-16*z(i-1)+5*z(i-2));
+    zz(i+1) = f(x(i+1), yy(i+1)) ;
+    y(i+1) = y(i) + k*(5*zz(i+1) + 8*z(i) - z(i-1)) ;
+    error_est(i+1) = -0.1*(y(i+1) - yy(i+1));
+    
+end
